8d8d0e74dbf26416803eae21035b4e658b167ec3
masum mondal <masummondal55355@gmail.com>
2023-10-07 10:51:47 +0530

    client setup

--- src/UserRegistration/client.js
+++ src/UserRegistration/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://uxpfusdvhvgyaynjeozq.supabase.co";
+const supabaseUrl = "https://ecqnzpihycmnsgsqnkrc.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4cGZ1c2R2aHZneWF5bmplb3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUyODU5MjgsImV4cCI6MjAxMDg2MTkyOH0.qQ2qpXuqkI5ZypcqopVVnNG-x1hPDIqI0pJwzJEYP3I";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjcW56cGloeWNtbnNnc3Fua3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2NTQyMTEsImV4cCI6MjAxMjIzMDIxMX0.mcpLW8KQKXcqfdtYcu6dKMxlxAHYfBWxFi7aWKeXiBs";
 export const supabase = createClient(supabaseUrl, supabaseKey);
